8410ce08f79bea8923f16bc35dc1d0f2301d1f77
shakermahamud <admin@shakermahamud.com>
2022-12-27 10:47:34 +0600

    code add

--- js/array.js
+++ js/array.js
@@ -1,7 +1,12 @@
 var friendNames = ['sakib','sabbir','rafiq','tusher','rabbi']
 
-console.log(friendNames.length)
-console.log(friendNames.indexOf('rafiq'))
-console.log(friendNames[0])
-friendNames[4] = 'Raffi'
-console.log(friendNames)
+// console.log(friendNames.length)
+// console.log(friendNames.indexOf('rafiq'))
+// console.log(friendNames[0])
+// friendNames[4] = 'Raffi'
+// console.log(friendNames)
+// friendNames.unshift('shaker')
+// friendNames.push('SHAKER')
+// friendNames.shift()
+// friendNames.pop()
+// console.log(friendNames)
---
+++ js/loop.js
@@ -0,0 +1,7 @@
+let number = 1
+while(number<50){
+    if(number%2==0){
+        console.log(number)
+    }
+    number++
+}
