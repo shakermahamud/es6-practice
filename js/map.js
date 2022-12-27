ef5fca0fb809d355d9ee04eb73c973b9d1c19d81
shakermahamud <admin@shakermahamud.com>
2022-12-27 19:51:14 +0600

    some map code add

---
+++ js/map.js
@@ -0,0 +1,34 @@
+let myMap = new Map()
+
+myMap.set('item_one','Book')
+myMap.set('item_two','Table')
+myMap.set('item_three','Chair')
+myMap.set('item_four','Computer')
+myMap.set('item_five','Mobile')
+myMap.set('item_six','Light')
+
+console.log(myMap.keys())
+console.log(myMap.values())
+
+for( mykeys of myMap.keys()){
+    console.log(mykeys)
+}
+for( myValues of myMap.values()){
+    console.log(myValues)
+}
+
+console.log(myMap.get('item_one'))
+
+myMap.delete('item_three')
+myMap.delete('item_four')
+console.log(myMap)
+
+myMap.clear()
+console.log(myMap)
+
+if(myMap.has('item_five')){
+    console.log('Yes')
+}else{
+    console.log('No')
+}
+
