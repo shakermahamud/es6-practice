9a527d561e28b3be5a1c0093df073d299b045de1
shakermahamud <admin@shakermahamud.com>
2022-12-27 11:02:09 +0600

    add loop code

--- js/loop.js
+++ js/loop.js
@@ -1,7 +1,13 @@
-let number = 1
-while(number<50){
-    if(number%2==0){
-        console.log(number)
-    }
-    number++
-}
+// let number = 1
+// while(number<50){
+//     if(number%2==0){
+//         console.log(number)
+//     }
+//     number++
+// }
+
+// let num = [1,2,3,4,5,6,7,8,9]
+
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i])
+// }
