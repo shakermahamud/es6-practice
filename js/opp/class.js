09ff853068654c5d4d527443aaed4245a1b5cd84
shakermahamud <admin@shakermahamud.com>
2022-12-27 21:19:30 +0600

    opp add

---
+++ js/opp/class.js
@@ -0,0 +1,21 @@
+class myClass {
+    myName(name){
+        console.log("My name is "+name)
+    }
+    myAge(age){
+        console.log('My age is '+age)
+    }
+    myHeight(height){
+        console.log('My height is '+height)
+    }
+    myPropession(propession){
+        console.log('My proprssion is '+propession)
+    }
+
+}
+let obj = new myClass;
+
+obj.myName('shaker mahamud')
+obj.myAge(23)
+obj.myHeight('5``6')
+obj.myPropession('Wordpress Website Deigner')
