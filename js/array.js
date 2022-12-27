07e7c827fa8f7f072ff3fe598bf66c8d79178bf2
shakermahamud <admin@shakermahamud.com>
2022-12-27 10:05:11 +0600

    some js code add

---
+++ js/array.js
@@ -0,0 +1,7 @@
+var friendNames = ['sakib','sabbir','rafiq','tusher','rabbi']
+
+console.log(friendNames.length)
+console.log(friendNames.indexOf('rafiq'))
+console.log(friendNames[0])
+friendNames[4] = 'Raffi'
+console.log(friendNames)
